a2c45ffc728ca5a21f01277968841127e0d6c9a4
Saamya Tyagi <karmendratyagi@Saamyas-MacBook-Pro.local>
2025-06-25 12:24:30 +0530

    rout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ScheduleFormComponent } from './schedule-form/schedule-form.component';   
+import { UpcomingAppointmentsComponent } from './upcoming-appointments/upcoming-appointments.component'; 
+import { PreviousAppointmentsComponent } from './previous-appointments/previous-appointments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: UpcomingAppointmentsComponent },
+  { path: 'schedule', component: ScheduleFormComponent },
+  { path: 'previous', component: PreviousAppointmentsComponent }
+];
--- src/app/upcoming-appointments/upcoming-appointments.component.ts
+++ src/app/upcoming-appointments/upcoming-appointments.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-upcoming-appointments',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './upcoming-appointments.component.html',
   styleUrl: './upcoming-appointments.component.css'
 })
 export class UpcomingAppointmentsComponent {
+  constructor(private router: Router) {}
 
+  navigateToSchedule() {
+    this.router.navigate(['/schedule']);
+  }
+  navigateToPreviousTab() {
+    this.router.navigate(['/previous']);
+  }
 }
